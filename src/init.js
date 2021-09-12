419d7614f38c96e35f5d85c466ff96d89e05f277
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-09-12 11:22:44 +0700

    add yup and onChange dependies to project.Add validate form

--- src/init.js
+++ src/init.js
@@ -1,9 +1,47 @@
 // @ts-check
 
-import Example from './Example.js';
+import * as yup from 'yup';
+import onChange from 'on-change';
 
 export default () => {
-  // const element = document.getElementById('point');
-  // const obj = new Example(element);
-  // obj.init();
+  const schema = yup.object().shape({
+    url: yup.string().url(),
+  });
+
+  const state = {
+    rssForm: {
+      valid: true,
+      processState: 'filling',
+      processError: null,
+      errors: [],
+      fields: {
+        url: '',
+      },
+    },
+  };
+
+  const rssForm = document.querySelector('.rss-form');
+  const urlInput = rssForm.querySelector('input[aria-label="url"]');
+  const rssBtn = rssForm.querySelector('button[aria-label="add"]');
+
+  urlInput.addEventListener('input', (e) => {
+    const { value } = e.target;
+    state.rssForm.fields.url = value;
+    schema
+      .isValid(state.rssForm.fields)
+      .then((isValidValue) => {
+        state.rssForm.valid = isValidValue;
+      })
+      .catch((error) => {
+        state.rssForm.processState = error.name;
+        state.rssForm.errors = [...state.rssForm.errors, ...error.errors];
+      });
+  });
+
+  const watchedState = onChange(state, (path, currentValue, prevValue) => {
+    console.log(path);
+    if (path === 'rssForm.valid') {
+      console.log('!!!');
+    }
+  });
 };
