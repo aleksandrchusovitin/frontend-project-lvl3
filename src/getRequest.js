676b10554d85524fc2fce96a3b9455f2ee847a49
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-09-22 14:53:06 +0700

    fix logics parser

--- src/getRequest.js
+++ src/getRequest.js
@@ -3,17 +3,23 @@ import _ from 'lodash';
 import parser from './parser.js';
 
 export default (url, watchedState, state) => {
+  const getNewItems = (currentItems, itemsInState, diffValue) => (
+    _.differenceBy(currentItems, itemsInState, diffValue));
+
   const newWatchedState = watchedState; // Понимаю, что это очень плохо :)
   const tick = () => {
     axios.get(`https://hexlet-allorigins.herokuapp.com/get?disableCache=true&url=${url}`)
       .then((data) => {
         const { feed, posts } = parser(url, data);
 
-        const newFeeds = _.differenceBy([feed], state.feeds, 'url');
-        const newPosts = _.differenceBy(posts, state.posts, 'link');
+        const newFeeds = getNewItems([feed], state.feeds, 'url');
+        const newPosts = getNewItems(posts, state.posts, 'link');
 
-        newWatchedState.feeds.push(...newFeeds);
-        newWatchedState.posts.push(...newPosts);
+        const newFeedsWithId = newFeeds.map((newFeed) => ({ ...newFeed, id: _.uniqueId() }));
+        const newPostsWithId = newPosts.map((newPost) => ({ ...newPost, id: _.uniqueId() }));
+
+        newWatchedState.feeds.push(...newFeedsWithId);
+        newWatchedState.posts.push(...newPostsWithId);
       })
       .catch((error) => {
         newWatchedState.rssForm.valid = false;
--- src/parser.js
+++ src/parser.js
@@ -1,16 +1,12 @@
-import _ from 'lodash';
-
 export default (url, data) => {
   const content = data.data.contents;
   const parser = new DOMParser();
   const doc = parser.parseFromString(content, 'text/html');
 
-  const feedId = _.uniqueId();
   const feedName = doc.querySelector('channel title').textContent;
   const feedDescription = doc.querySelector('channel description').textContent;
 
   const feed = {
-    id: feedId,
     name: feedName,
     description: feedDescription,
     url,
@@ -18,14 +14,11 @@ export default (url, data) => {
 
   const items = doc.querySelectorAll('item');
   const posts = Array.from(items).map((item) => {
-    const itemId = _.uniqueId();
     const itemName = item.querySelector('title').textContent;
     const itemLink = item.querySelector('link').nextSibling.textContent;
     const itemDescription = item.querySelector('description').textContent;
 
     return {
-      id: itemId,
-      feedId,
       name: itemName,
       description: itemDescription,
       link: itemLink,
